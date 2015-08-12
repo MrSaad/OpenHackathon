5193d290a7a5d91a6c4c9db5b65a3baa71930a61
Saad Ahmed <saadem3000@gmail.com>
2015-08-11 20:50:28 -0400

    Initial Commit

---
+++ OpenHackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenHackathon
+//
+//  Created by Saad Ahmed on 2015-08-11.
+//  Copyright (c) 2015 Saad Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
